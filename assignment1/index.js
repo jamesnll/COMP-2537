2e499de3ee8c1984d484a04749722619fcf953f9
jamesnll <langillejames11@gmail.com>
2023-04-25 16:44:28 -0700

    Added db connection

--- assignment1/index.js
+++ assignment1/index.js
@@ -1,12 +1,12 @@
-/** Requirements. */
+/** Required modules. */
 require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
-const mongoStore = require('connect-mongodb-session');
+const mongoDB = require('connect-mongo');
 const bcrypt = require('bcrypt');
 const joi = require('joi');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3020;
 const app = express();
 
 
@@ -18,16 +18,31 @@ const mongodb_database = process.env.mongodb_database;
 const mongodb_secretSession = process.env.MONGODB_SESSION_SECRET;
 const mongodb_nodeSecretSession = process.env.NODE_SESSION_SECRET;
 
+var {database} = require('./databaseConnection.js');
+
+const userCollection = database.db(mongodb_database).collection('users');
+
+app.use(express.urlencoded({extended: false}));
+
+var mongoStore = mongoDB.create({
+	mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/Sessions`,
+	crypto: {
+		secret: mongodb_secretSession
+	}
+})
+
 app.use(session({
     secret: mongodb_secretSession,
-	// store: mongoStore, //default is memory store
+	store: mongoStore, //default is memory store
 	saveUninitialized: false,
 	resave: true
 }
-));
+))
 
 app.get('/', (req, res) => {
-    var html = "<a href='/signup'>Sign Up</a><br/>" + "<button>Log In</button>";
+    var html = `
+        <button onclick="window.location.href='/login'">Login</button><br/>
+        <button onclick="window.location.href='/signup'">Sign up</button>`;
     res.send(html);
 })
 
